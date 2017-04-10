8399b1e2fae36e45c72796b6667605b3662584e8
Stefan <stefke2105996@gmail.com>
2017-04-10 18:16:51 +0200

    Config.java is now package-private

--- app/src/main/java/rs/sdee/aplikacija/Config.java
+++ app/src/main/java/rs/sdee/aplikacija/Config.java
@@ -1,50 +1,47 @@
 package rs.sdee.aplikacija;
 
-/**
- * Created by Stefan on 3.7.2016.
- */
-public class Config {
+class Config {
 
     //Address of scripts
-    public static final String URL_ADD="http://stefan.oblacic.oo.gd/scriptsAdmin/addMatch.php";
-    public static final String URL_GET_ALL = "http://stefan.oblacic.oo.gd/scriptsAdmin/getAll.php";
-    public static final String URL_GET_MATCH = "http://stefan.oblacic.oo.gd/scriptsAdmin/getMatch.php?id=";
-    public static final String URL_UPDATE_MATCH = "http://stefan.oblacic.oo.gd/scriptsAdmin/updateLista.php";
-    public static final String URL_DELETE_MATCH = "http://stefan.oblacic.oo.gd/scriptsAdmin/delMatch.php";
+    static final String URL_ADD="http://stefan.oblacic.oo.gd/scriptsAdmin/addMatch.php";
+    static final String URL_GET_ALL = "http://stefan.oblacic.oo.gd/scriptsAdmin/getAll.php";
+    static final String URL_GET_MATCH = "http://stefan.oblacic.oo.gd/scriptsAdmin/getMatch.php?id=";
+    static final String URL_UPDATE_MATCH = "http://stefan.oblacic.oo.gd/scriptsAdmin/updateLista.php";
+    static final String URL_DELETE_MATCH = "http://stefan.oblacic.oo.gd/scriptsAdmin/delMatch.php";
 
     //Keys that will be used to send the request to php scripts
-    public static final String KEY_LIGA = "liga";
-    public static final String KEY_SIFRA = "sifra";
-    public static final String KEY_UTAKMICA = "utakmica";
-    public static final String KEY_KI1 = "ki1";
-    public static final String KEY_KIX = "kix";
-    public static final String KEY_KI2 = "ki2";
-    public static final String KEY_UG02 = "ug02";
-    public static final String KEY_UG3P = "ug3p";
-    public static final String KEY_SPECIJAL = "specijal";
+    static final String KEY_LIGA = "liga";
+    static final String KEY_SIFRA = "sifra";
+    static final String KEY_UTAKMICA = "utakmica";
+    static final String KEY_KI1 = "ki1";
+    static final String KEY_KIX = "kix";
+    static final String KEY_KI2 = "ki2";
+    static final String KEY_UG02 = "ug02";
+    static final String KEY_UG3P = "ug3p";
+    static final String KEY_SPECIJAL = "specijal";
 
     //JSON Tags
-    public static final String TAG_JSON_ARRAY = "result";
-    public static final String TAG_LIGA = "liga";
-    public static final String TAG_SIFRA = "sifra";
-    public static final String TAG_UTAKMICA = "utakmica";
-    public static final String TAG_KI1 = "ki1";
-    public static final String TAG_KIX = "kix";
-    public static final String TAG_KI2 = "ki2";
-    public static final String TAG_UG02 = "ug02";
-    public static final String TAG_UG3P = "ug3p";
-    public static final String TAG_SPECIJAL = "specijal";
+    static final String TAG_JSON_ARRAY = "result";
+    static final String TAG_LIGA = "liga";
+    static final String TAG_SIFRA = "sifra";
+    static final String TAG_UTAKMICA = "utakmica";
+    static final String TAG_KI1 = "ki1";
+    static final String TAG_KIX = "kix";
+    static final String TAG_KI2 = "ki2";
+    static final String TAG_UG02 = "ug02";
+    static final String TAG_UG3P = "ug3p";
+    static final String TAG_SPECIJAL = "specijal";
 
     //sifra to pass with intent
-    public static final String LIGA = "liga";
-    public static final String SIFRA = "sifra";
-    public static final String UTAKMICA = "utakmica";
-    public static final String KI1 = "ki1";
-    public static final String KIX = "kix";
-    public static final String KI2 = "ki2";
-    public static final String UG02 = "ug02";
-    public static final String UG3P = "ug3p";
-    public static final String SPECIJAL = "specijal";
+    static final String LIGA = "liga";
+    static final String SIFRA = "sifra";
+    static final String UTAKMICA = "utakmica";
+    static final String KI1 = "ki1";
+    static final String KIX = "kix";
+    static final String KI2 = "ki2";
+    static final String UG02 = "ug02";
+    static final String UG3P = "ug3p";
+    static final String SPECIJAL = "specijal";
 
 
 }
